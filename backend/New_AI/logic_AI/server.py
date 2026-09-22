35bae250df98b32c086641a3518131973b708118
Nguyen Viet Bach <Vietbachnguyen95@gmail.com>
2026-09-22 21:53:36 +0700

    new chatbot

--- backend/New_AI/logic_AI/server.py
+++ backend/New_AI/logic_AI/server.py
@@ -62,4 +62,4 @@ async def chat_endpoint(request: ChatRequest):
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=False)
