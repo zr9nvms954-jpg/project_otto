e417dde853d109c1fbe783c5608fdc0697774cb5
NguyenHaiAnh06 <tqx4nb4j7r@privaterelay.appleid.com>
2026-09-23 08:27:46 +0700

    local host

--- frontend/java/auth.js
+++ frontend/java/auth.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'http://localhost:5001/api/auth';
 
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('container');
